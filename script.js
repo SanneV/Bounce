d56e5ec8f16299c2765a91a91ae32055c0d31dd0
SanneV <sanneversl@gmail.com>
2021-09-17 09:27:30 +0000

    code overgenomen

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-
-/**
- * variabelen
- * hier staan alle variabelen waarvan gebruik wordt gemaakt
- */
-
-var x = [25, 25, 25, 25, 25];
-var y = [25, 25, 25, 25, 25];
-var speedX = [5, 4, 3, 2, 1];
-var speedY = [5, 4, 3, 2, 1];
-
+// globale variabelen
+var ballen = [];
 
 /**
  * setup
@@ -16,93 +7,23 @@ var speedY = [5, 4, 3, 2, 1];
  * de p5 library, zodra het spel geladen is in de browser
  */
 function setup() {
-  
-}
+  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  createCanvas(1280, 720);
 
-/**
- * alle functies
- * alle balletjes (1 t/m 5) hebben allemaal een eigen 
- * functie met eigen variabelen
- */
+  for (var i = 0; i < 25; i++) {
+    var randomx = random(50, 1230);
+    var randomy = random(50, 670);
+    var randomSpeedX = random (-5, 5);
+    var randomSpeedY = random (-5, 5);
 
-var balletje1 = function(){
-  fill(255, 0, 0);
-  ellipse(x[0], y[0], 50, 50);
+    var bal = new Bal(randomx, randomy, randomSpeedX, randomSpeedY);
 
-  y[0] = y[0] + speedY[0];
-  x[0] = x[0] + speedX[0];
-
-  if(y[0] > 695 || y[0] < 25){
-    speedY[0] = speedY[0] * -1;
+    ballen.push(bal);
   }
-  if(x[0] > 1255 || x[0] < 25){
-    speedX[0] = speedX[0] * -1;
-    }
-}
 
-var balletje2 = function(){
-  fill(200, 200, 0);
-  ellipse(x[1], y[1], 50, 50);
-
-  y[1] = y[1] + speedY[1];
-  x[1] = x[1] + speedX[1];
-
-  if(y[1] > 695 || y[1] < 25){
-    speedY[1] = speedY[1] * -1;
-  }
-  if(x[1] > 1255 || x[1] < 25){
-    speedX[1] = speedX[1] * -1;
-    }
 
 }
 
-var balletje3 = function(){
-  fill(0, 255, 0);
-  ellipse(x[2], y[2], 50, 50);
-
-  y[2] = y[2] + speedY[2];
-  x[2] = x[2] + speedX[2];
-
-  if(y[2] > 695 || y[2] < 25){
-    speedY[2] = speedY[2] * -1;
-  }
-  if(x[2] > 1255 || x[2] < 25){
-    speedX[2] = speedX[2] * -1;
-    }
-
-}
-
-var balletje4 = function(){
-  fill(0, 0, 255);
-  ellipse(x[3], y[3], 50, 50);
-
-  y[3] = y[3] + speedY[3];
-  x[3] = x[3] + speedX[3];
-
-  if(y[3] > 695 || y[3] < 25){
-    speedY[3] = speedY[3] * -1;
-  }
-  if(x[3] > 1255 || x[3] < 25){
-    speedX[3] = speedX[3] * -1;
-    }
-
-}
-
-var balletje5 = function(){
-  fill(200, 0, 200);
-  ellipse(x[4], y[4], 50, 50);
-
-  y[4] = y[4] + speedY[4];
-  x[4] = x[4] + speedX[4];
-
-  if(y[4] > 695 || y[4] < 25){
-    speedY[4] = speedY[4] * -1;
-  }
-  if(x[4] > 1255 || x[4] < 25){
-    speedX[4] = speedX[4] * -1;
-    }
-
-}
 
 /**
  * draw
@@ -110,21 +31,11 @@ var balletje5 = function(){
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
 
-  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
-  createCanvas(1280, 720);
-
-  // Kleur de achtergrond zwart, zodat je het kunt zien
-  background(0, 0, 0);
-
-  // Kleur van de omlijning van de balletjes
-  stroke(255, 255, 255);
-
-  // Hier worden alle functies van de 5 balletjes aangeroepen
-  balletje1();
-  balletje2();
-  balletje3();
-  balletje4();
-  balletje5();
-
+  for(var i = 0; i < ballen.length; i++) {
+    ballen[i].show();
+    ballen[i].update();
+  }
 }
